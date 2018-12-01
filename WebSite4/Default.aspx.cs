c664f859b7eebd0bce2787f393660fbea20880ee
Anay Khator <33987976+Anaykhator@users.noreply.github.com>
2018-12-01 18:27:29 +0530

    Initial Commit

---
+++ WebSite4/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+        }
+    }
+
+    public void btn1(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        string cat = DDL1.SelectedItem.Text.ToString();
+        string lat = hdn.Value;
+        string lng =hdn2.Value;
+        TextBox1.Text = lng;
+        
+        con.ConnectionString = "Data Source=(local);Initial Catalog=MyGov;Integrated Security=True";
+        string sqlQuery = "INSERT INTO Complaints select ISNULL(Max(Cid),100)+1 ,'"+cat+"','"+lat+"','"+lng+"','"+txtPhotoImg.Text.ToString()+"' from Complaints";
+
+        SqlCommand comm = new SqlCommand(sqlQuery, con);
+        con.Open();
+        
+        
+        int rows = comm.ExecuteNonQuery();
+        if (rows > 0)
+        {
+            TextBox1.Text = "Done";
+            
+        }
+        con.Close();
+        //TextBox1.Text = cat;
+    }
+
+    protected void btnUpload_Click3(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile == true)
+        {
+            string fileNameMaster = (System.DateTime.Now.ToString("ddmmyyyyhhssf") + FileUpload1.FileName);
+            string savePath = (Server.MapPath("Img/") + fileNameMaster.ToString());
+
+            FileUpload1.SaveAs(savePath);
+            txtPhotoImg.Text = fileNameMaster.ToString();
+            Img1.ImageUrl = "Img/" + fileNameMaster.ToString();
+            Img1.Visible = true;
+        }
+    }
+    
+}
---
+++ WebSite4/Default2.aspx
@@ -0,0 +1,37 @@
+﻿
+
+
+<%@ Page Language="C#" AutoEventWireup="false" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<head id="Head1" runat="server">
+    <title></title>
+
+<script type="text/javascript"
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBZUzE6FSpdGkntaWxx1xFsIqoII0Dfalc&sensor=false">
+</script>
+
+<script type="text/javascript">
+ function intialize(lat,lon)
+ {
+ var myLatlng=new google.maps.Lating(lat,lon)
+  var mapOptions={
+   center:myLating,
+   zoom:20,
+   mapTypeId:google.maps.MapType.ROADMAP,
+   marker:true};
+   var map=new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+   var marker=new google.maps.Marker({position:myLatlng});marker.setMap(map0;
+   }
+   window.onload=function(){intialize('25.333663','74.6403287')};
+   </script>
+
+ 
+</head>
+<body>
+  
+    <form id="form1" runat="server">
+<div id="map_canvas" style="width: 500px; height: 400px"></div>
+</form>
+</body>
+</html>
